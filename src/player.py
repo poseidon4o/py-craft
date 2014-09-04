7aa62983bd387cb2103b07edcd361a4c21e6479e
poseidon4o <poseidon4o.random@gmail.com>
2014-09-04 19:13:14 +0300

    Chage player movement to be velocity driven

--- src/player.py
+++ src/player.py
@@ -1,13 +1,16 @@
 from .world.world import World
 from sdl2 import timer
-
+from .utils import Coord, signof
 
 class Player:
     
     def __init__(self, world, sprite):
+        self.position = Coord()
+
         self.sprite = sprite
-        self.size = (1, 2)
-        self.speed = 4
+        self.size = Coord(1, 2)
+        self.speed = 2
+        self.velocity = Coord(0, self.speed)
 
         self.world = world
         self.last_tick = timer.SDL_GetTicks()
@@ -16,32 +19,41 @@ class Player:
 
     # position self at center of the world
     def reposition(self):
-        self.position = [self.world.width // 2, 0]
+        self.position.pos = [self.world.width // 2, 10]
         self.tick()
 
     def tick(self):
-        if timer.SDL_GetTicks() - self.last_tick < 100:
-            return
+        if self.world[self.position.x][self.position.y + self.size.y].solid and self.velocity.y > 0:
+            self.velocity.y = 0
+        else:
+            self.velocity.y += 1
 
-        self.last_tick = timer.SDL_GetTicks()
 
-        for y in self.world.pointed_range(self.position[1] + self.size[1], self.position[1] + self.size[1] + int(self.speed * 0.5)):
-            if not self.world[self.position[0]][y].solid:
-                self.position[1] += 1
+        x_direction = signof(self.velocity.x)
+
+        for x in self.world.pointed_range(self.position.x + self.size.x * (x_direction == 1), self.position.x + int(self.velocity.x) + self.size.x * (x_direction == 1)):
+            solid = False
+            for y in self.world.in_height(self.position.y, self.position.y + self.size.y):
+                solid = self.world[x][y].solid or solid
+
+            if not solid:
+                self.position.x += 1 * x_direction
+                self.velocity.x -= 1 * x_direction
+            else:
+                self.velocity.x = 0
+                break
+
+        y_direction = signof(self.velocity.y)
+        for y in self.world.pointed_range(self.position.y + self.size.y * (y_direction == 1), self.position.y + self.size.y * (y_direction == 1) + int(self.velocity.y)):
+            if not self.world[self.position.x][y].solid:
+                self.position.y += 1 * y_direction
 
     def jump(self):
         #no air jumping
         if not self.world[self.position[0]][self.position[1] + self.size[1]].solid:
             return
 
-        self.last_tick = timer.SDL_GetTicks()
-        for y in self.world.pointed_range(self.position[1], self.position[1] + self.speed * 5):
-            if not self.world[self.position[0]][y].solid:
-                self.position[1] -= 1
+        self.velocity.y -= self.speed * 2
 
-    def move(self, disp):
-        for x in self.world.pointed_range(self.position[0] + (disp == 1), self.position[0] + self.speed * disp):
-            for y in range(self.size[1]):
-                if self.world[x][self.position[1] + y].solid:
-                    return
-            self.position[0] += disp
+    def move(self, direction):
+        self.velocity.x += direction * self.speed
