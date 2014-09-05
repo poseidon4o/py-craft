60dce367c7bd5ce89010a21c51d49e0a32abed5c
poseidon4o <poseidon4o.random@gmail.com>
2014-09-05 19:07:50 +0300

    Add dirty flag and prepare for smoother possition

--- src/player.py
+++ src/player.py
@@ -1,11 +1,14 @@
 from .world.world import World
 from sdl2 import timer
-from .utils import Coord, signof
+from .utils import Coord, signof, ceil_abs
 
 class Player:
     
     def __init__(self, world, sprite):
         self.position = Coord()
+
+        self.dirty = True
+        self.dirty_rect = (0, 0, 0, 0)
 
         self.sprite = sprite
         self.size = Coord(1, 2)
@@ -19,34 +22,49 @@ class Player:
 
     # position self at center of the world
     def reposition(self):
-        self.position.pos = [self.world.width // 2, 10]
+        self.position.pos = [self.world.width / 2, 10]
         self.tick()
 
     def tick(self):
-        if self.world[self.position.x][self.position.y + self.size.y].solid and self.velocity.y > 0:
-            self.velocity.y = 0
-        else:
-            self.velocity.y += 1
+        prev_pos = Coord(*self.position.pos)
 
+        if not self.world[self.position.x][self.position.y + self.size.y].solid:
+            self.velocity.y += 1
+        elif self.world[self.position.x][self.position.y - 1].solid and self.velocity.y < 0:
+            self.velocity.y = 0
+        elif self.velocity.y > 0:
+            self.velocity.y = 0
 
         x_direction = signof(self.velocity.x)
 
-        for x in self.world.pointed_range(self.position.x + self.size.x * (x_direction == 1), self.position.x + int(self.velocity.x) + self.size.x * (x_direction == 1)):
+        for x in self.world.pointed_range(self.position.x + self.size.x * (x_direction == 1), self.position.x + ceil_abs(self.velocity.x) + self.size.x * (x_direction == 1)):
             solid = False
             for y in self.world.in_height(self.position.y, self.position.y + self.size.y):
                 solid = self.world[x][y].solid or solid
 
             if not solid:
-                self.position.x += 1 * x_direction
-                self.velocity.x -= 1 * x_direction
+                step = x_direction if abs(self.velocity.x) >= 1 else self.velocity.x
+                self.position.x += step
+                self.velocity.x -= step
             else:
                 self.velocity.x = 0
                 break
 
         y_direction = signof(self.velocity.y)
-        for y in self.world.pointed_range(self.position.y + self.size.y * (y_direction == 1), self.position.y + self.size.y * (y_direction == 1) + int(self.velocity.y)):
+        for y in self.world.pointed_range(self.position.y + self.size.y * (y_direction == 1), self.position.y + self.size.y * (y_direction == 1) + self.velocity.y):
             if not self.world[self.position.x][y].solid:
                 self.position.y += 1 * y_direction
+
+        if prev_pos != self.position:
+            self.dirty = True
+            self.dirty_rect = (
+                min(prev_pos.x, self.position.x),
+                min(prev_pos.y, self.position.y),
+                max(prev_pos.x, self.position.x) + self.size.x,
+                max(prev_pos.y, self.position.y) + self.size.y
+            )
+        else:
+            self.dirty = False
 
     def jump(self):
         #no air jumping
